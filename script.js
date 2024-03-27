87dfcf0457b7f955d21b2a8f396e055559cf832d
NIKITH KUMAR N <nikithkumarniki160698@gmail.com>
2024-03-27 22:36:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+let panel = document.querySelectorAll(".panel")
+panel.forEach(panel=>{
+  panel.addEventListener("click",(e)=>{
+    removing();
+    panel.classList.add("active")
+  })
+})
+
+function removing(){
+  panel.forEach(panel=>{
+    panel.classList.remove("active");
+  })
+}
